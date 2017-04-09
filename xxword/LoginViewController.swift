8ad065df74b2069d2ec388fe417f59fa406d864b
Zhang LiangZhi <3717591@qq.com>
2017-04-10 01:18:49 +0800

    o

--- xxword/LoginViewController.swift
+++ xxword/LoginViewController.swift
@@ -14,6 +14,8 @@ class LoginViewController: UIViewController {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        self.view.backgroundColor = UIColor.white
+        initLgv()
     }
 
     override func didReceiveMemoryWarning() {
@@ -21,15 +23,8 @@ class LoginViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    func initLgv() {
+        
     }
-    */
 
 }
--- xxword/ViewController.swift
+++ xxword/ViewController.swift
@@ -13,6 +13,22 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        print("h")
+        let btn = UIButton(type: .contactAdd)
+        self.view.addSubview(btn)
+        btn.setTitle("Go", for: .normal)
+        btn.frame = CGRect(x: 88, y: 88, width: 88, height: 88)
+        btn.addTarget(self, action: #selector(btnGo), for: .touchUpInside)
+//        btn.center = self.view.center
+        
+        
+        let button:UIButton = UIButton(type: .system)
+        //设置按钮位置和大小
+        button.frame = CGRect(x:10, y:150, width:100, height:30)
+        //设置按钮文字
+        button.setTitle("按钮", for:.normal)
+        self.view.addSubview(button)
     }
 
     override func didReceiveMemoryWarning() {
@@ -21,5 +37,11 @@ class ViewController: UIViewController {
     }
 
 
+    func btnGo() {
+//        self.present(LoginViewController(), animated: true) { 
+//            print("hi")
+//        }
+        self.navigationController?.pushViewController(LoginViewController(), animated: true)
+    }
 }
 
