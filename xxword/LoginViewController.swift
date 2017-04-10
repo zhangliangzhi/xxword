a999c06b0066b8ab744e044ca45388b8a0beccb8
Zhang LiangZhi <3717591@qq.com>
2017-04-10 12:01:19 +0800

    o

--- xxword/LoginViewController.swift
+++ xxword/LoginViewController.swift
@@ -7,16 +7,25 @@
 //
 
 import UIKit
+import SnapKit
 
-class LoginViewController: UIViewController {
+class LoginViewController: UIViewController, UITextFieldDelegate {
 
+    var v:UIView!
     var outNumTextField:UITextField!
     var outPwdTextField:UITextField!
     var outLoginButton:UIButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
+        v = UIView()
+        self.view.addSubview(v)
+        v.snp.makeConstraints { (make) in
+            make.width.height.equalTo(self.view)
+            make.center.equalTo(self.view)
+        }
+        v.backgroundColor = UIColor.darkGray
         // Do any additional setup after loading the view.
         self.view.backgroundColor = UIColor.white
         initLgv()
@@ -30,8 +39,54 @@ class LoginViewController: UIViewController {
     func initLgv() {
         // phone number
         outNumTextField = UITextField()
-        self.view.addSubview(outNumTextField)
+        v.addSubview(outNumTextField)
+        outNumTextField.snp.makeConstraints { (make) in
+            make.width.equalTo(v).multipliedBy(0.8)
+            make.height.equalTo(40)
+            make.top.equalTo(v).offset(150)
+            make.centerX.equalTo(v)
+        }
+        outNumTextField.delegate = self
+        outNumTextField.keyboardType = .numbersAndPunctuation
+        outNumTextField.spellCheckingType = .no
+        outNumTextField.autocorrectionType = .no
+        outNumTextField.returnKeyType = .next
+        outNumTextField.borderStyle = .roundedRect
+        outNumTextField.backgroundColor = UIColor.white
+        outNumTextField.autocapitalizationType = .none
+        outNumTextField.placeholder = "输入手机号码"
         
+        outPwdTextField = UITextField()
+        v.addSubview(outPwdTextField)
+        outPwdTextField.snp.makeConstraints { (make) in
+            make.centerX.equalTo(v)
+            make.width.equalTo(v).multipliedBy(0.8)
+            make.height.equalTo(40)
+            make.top.equalTo(outNumTextField.snp.bottom).offset(8)
+        }
+        outPwdTextField.delegate = self
+        outPwdTextField.keyboardType = .asciiCapable
+        outPwdTextField.spellCheckingType = .no
+        outPwdTextField.autocorrectionType = .no
+        outPwdTextField.returnKeyType = .done
+        outPwdTextField.borderStyle = .roundedRect
+        outPwdTextField.backgroundColor = UIColor.white
+        outPwdTextField.autocapitalizationType = .none
+        outPwdTextField.placeholder = "输入密码"
+        
+        // image
+        
+        let numImage = UIImageView(frame: CGRect(x: 6, y: 0, width: 25, height: 25))
+        numImage.image = UIImage(named: "number")
+        outNumTextField.leftView = UIView(frame: CGRect(x: 0, y: 0, width: 30, height: 25))
+        outNumTextField.leftView?.addSubview(numImage)
+        outNumTextField.leftViewMode = .always
+
+        let pwdImage = UIImageView(frame: CGRect(x: 6, y: 0, width: 25, height: 25))
+        pwdImage.image = UIImage(named: "password")
+        outPwdTextField.leftView = UIView(frame: CGRect(x: 0, y: 0, width: 30, height: 25))
+        outPwdTextField.leftViewMode = .always
+        outPwdTextField.leftView?.addSubview(pwdImage)
     }
 
 }
