36f6ca53fc8eb5feed07c5087e430d3703616a73
Zhang LiangZhi <3717591@qq.com>
2017-04-10 19:36:24 +0800

    o

--- xxword/LoginViewController.swift
+++ xxword/LoginViewController.swift
@@ -25,7 +25,7 @@ class LoginViewController: UIViewController, UITextFieldDelegate {
             make.width.height.equalTo(self.view)
             make.center.equalTo(self.view)
         }
-        v.backgroundColor = UIColor.darkGray
+//        v.backgroundColor = UIColor.darkGray
         // Do any additional setup after loading the view.
         self.view.backgroundColor = UIColor.white
         initLgv()
@@ -36,6 +36,11 @@ class LoginViewController: UIViewController, UITextFieldDelegate {
         // Dispose of any resources that can be recreated.
     }
     
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        outNumTextField.resignFirstResponder()
+        outPwdTextField.resignFirstResponder()
+    }
+    
     func initLgv() {
         // phone number
         outNumTextField = UITextField()
@@ -43,7 +48,7 @@ class LoginViewController: UIViewController, UITextFieldDelegate {
         outNumTextField.snp.makeConstraints { (make) in
             make.width.equalTo(v).multipliedBy(0.8)
             make.height.equalTo(40)
-            make.top.equalTo(v).offset(150)
+            make.top.equalTo(v).offset(120)
             make.centerX.equalTo(v)
         }
         outNumTextField.delegate = self
@@ -75,7 +80,6 @@ class LoginViewController: UIViewController, UITextFieldDelegate {
         outPwdTextField.placeholder = "输入密码"
         
         // image
-        
         let numImage = UIImageView(frame: CGRect(x: 6, y: 0, width: 25, height: 25))
         numImage.image = UIImage(named: "number")
         outNumTextField.leftView = UIView(frame: CGRect(x: 0, y: 0, width: 30, height: 25))
@@ -87,6 +91,62 @@ class LoginViewController: UIViewController, UITextFieldDelegate {
         outPwdTextField.leftView = UIView(frame: CGRect(x: 0, y: 0, width: 30, height: 25))
         outPwdTextField.leftViewMode = .always
         outPwdTextField.leftView?.addSubview(pwdImage)
+        
+        // close btn
+        let numBtn = UIButton(frame: CGRect(x: -1, y: 0, width: 25, height: 25))
+        numBtn.setBackgroundImage(UIImage(named: "close"), for: .normal)
+        outNumTextField.rightView = UIView(frame: CGRect(x: 0, y: 0, width: 30, height: 25))
+        outNumTextField.rightView?.addSubview(numBtn)
+        outNumTextField.rightViewMode = .whileEditing
+        numBtn.addTarget(self, action: #selector(delNumTextField), for: .touchUpInside)
+        
+        let pwdBtn = UIButton(frame: CGRect(x: -1, y: 0, width: 25, height: 25))
+        pwdBtn.setBackgroundImage(UIImage(named: "close"), for: .normal)
+        outPwdTextField.rightView = UIView(frame: CGRect(x: 0, y: 0, width: 30, height: 25))
+        outPwdTextField.rightView?.addSubview(pwdBtn)
+        outPwdTextField.rightViewMode = .whileEditing
+        pwdBtn.addTarget(self, action: #selector(delPwdTextField), for: .touchUpInside)
+        
+        // first responder
+//        outNumTextField.becomeFirstResponder()
+        
+        // 登入按钮
+        outLoginButton = BootstrapBtn(frame: CGRect(x: 0, y: 0, width: 150, height: 40), btButtonType: .Success)
+        v.addSubview(outLoginButton)
+        outLoginButton.snp.makeConstraints { (make) in
+            make.width.equalTo(outPwdTextField)
+            make.height.equalTo(40)
+            make.centerX.equalTo(outPwdTextField)
+            make.top.equalTo(outPwdTextField.snp.bottom).offset(40)
+        }
+        outLoginButton.setTitle("登录", for: .normal)
+        outLoginButton.addTarget(self, action: #selector(btnGoSignIn), for: .touchUpInside)
+    }
+    
+    func delNumTextField() {
+        outNumTextField.text = ""
+    }
+    
+    func delPwdTextField() {
+        outPwdTextField.text = ""
+    }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        if textField == outNumTextField {
+            outPwdTextField.becomeFirstResponder()
+        }else if textField == outPwdTextField {
+            outPwdTextField.resignFirstResponder()
+        }
+        return true
     }
 
+    // 登入
+    func btnGoSignIn() {
+        
+    }
+    
+    // 注册
+    func btnGoSignUp() {
+        
+    }
 }
