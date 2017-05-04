bedd5ac2369dbe12bd0a36b4dee0c31a8cfb8257
Zhang LiangZhi <3717591@qq.com>
2017-05-05 02:51:22 +0800

    o

---
+++ xxword/ocr.h
@@ -0,0 +1,13 @@
+//
+//  ocr.h
+//  xxword
+//
+//  Created by ZhangLiangZhi on 2017/5/4.
+//  Copyright © 2017年 xigk. All rights reserved.
+//
+
+#ifndef ocr_h
+#define ocr_h
+
+
+#endif /* ocr_h */
---
+++ xxword/ocr.m
@@ -0,0 +1,9 @@
+//
+//  ocr.m
+//  xxword
+//
+//  Created by ZhangLiangZhi on 2017/5/4.
+//  Copyright © 2017年 xigk. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
