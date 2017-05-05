b1e21af129e0c3cdbb715623c73a4f92714ab85c
Zhang LiangZhi <3717591@qq.com>
2017-05-06 01:49:49 +0800

    o

---
+++ xxword/XxCollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  XxCollectionViewCell.swift
+//  xxword
+//
+//  Created by ZhangLiangZhi on 2017/5/5.
+//  Copyright © 2017年 xigk. All rights reserved.
+//
+
+import UIKit
+
+class XxCollectionViewCell: UICollectionViewCell {
+    
+}
