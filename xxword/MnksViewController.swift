6ec518aabde897a1b6f4b5e39825f4c99124055f
Zhang LiangZhi <3717591@qq.com>
2017-05-17 23:26:48 +0800

    o

---
+++ xxword/MnksViewController.swift
@@ -0,0 +1,35 @@
+//
+//  MnksViewController.swift
+//  xxword
+//
+//  Created by ZhangLiangZhi on 2017/5/17.
+//  Copyright © 2017年 xigk. All rights reserved.
+//
+
+import UIKit
+
+class MnksViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
