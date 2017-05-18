6e476c48362fc3dbe2c595c32eafcf374d446fa6
Zhang LiangZhi <3717591@qq.com>
2017-05-19 02:45:58 +0800

    o

--- xxword/KsxzViewController.swift
+++ xxword/KsxzViewController.swift
@@ -23,7 +23,7 @@ class KsxzViewController: UIViewController {
             make.centerY.equalTo(self.view)
             make.centerX.equalTo(self.view)
         }
-        nameLabel.text = "考试说明:\n考试时间为30/45分钟, 共50/100题,\n 时间结束自动提交成绩. 可查看成绩排名."
+        nameLabel.text = "考试说明:\n考试时间为30分钟, 共100题,\n 时间结束自动提交成绩. 可查看成绩排名."
         nameLabel.textColor = SX3_COLOR
         nameLabel.numberOfLines = 0
     }
